ca33161b760fb4bd60bf1cfcec69dfe791071be0
Alfredo Colangelo <6275539+acola@users.noreply.github.com>
2024-10-16 16:24:03 +0200

    fix serving favicon: it’s just a test, it’s not important for an API

--- src/fastapi_uv/main.py
+++ src/fastapi_uv/main.py
@@ -1,12 +1,25 @@
+import os
+
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
 def get_root() -> dict[str, str]:
     return {"message": "OK"}
+
+
+@app.get("/favicon.ico")
+async def favicon() -> FileResponse:
+    file_name = "favicon.ico"
+    file_path = os.path.join(app.root_path, "static", file_name)
+    return FileResponse(path=file_path, headers={"Content-Disposition": "attachment; filename=" + file_name})
 
 
 if __name__ == "__main__":
