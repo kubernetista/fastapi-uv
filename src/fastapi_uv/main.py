a1dfaa5552cb83eaa2f7b78585ab44f83398a9b8
Alfredo Colangelo <6275539+acola@users.noreply.github.com>
2024-10-21 22:10:27 +0200

    fix error with CTRL-C with docker run

--- src/fastapi_uv/main.py
+++ src/fastapi_uv/main.py
@@ -14,4 +14,4 @@ def get_root() -> dict[str, str]:
 if __name__ == "__main__":
     host = os.getenv("HOST", "0.0.0.0")  # noqa: S104  # nosec B104
     port = int(os.getenv("PORT", 8001))
-    uvicorn.run(app, host=host, port=port)
+    uvicorn.run(app, host=host, port=port, lifespan="off")
