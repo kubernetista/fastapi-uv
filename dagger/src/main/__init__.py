de63af6805a794d61fd328b5fc6ee2f363df3adc
Alfredo Colangelo <6275539+acola@users.noreply.github.com>
2024-10-22 16:16:25 +0200

    Dagger: add to the project

---
+++ dagger/src/main/__init__.py
@@ -0,0 +1,30 @@
+# NOTE: it's recommended to move your code into other files in this package
+# and keep __init__.py for imports only, according to Python's convention.
+# The only requirement is that Dagger needs to be able to import a package
+# called "main" (i.e., src/main/).
+#
+# For example, to import from src/main/main.py:
+# >>> from .main import FastapiUv as FastapiUv
+
+import dagger
+from dagger import dag, function, object_type
+
+
+@object_type
+class FastapiUv:
+    @function
+    def container_echo(self, string_arg: str) -> dagger.Container:
+        """Returns a container that echoes whatever string argument is provided"""
+        return dag.container().from_("alpine:latest").with_exec(["echo", string_arg])
+
+    # @function
+    # async def grep_dir(self, directory_arg: dagger.Directory, pattern: str) -> str:
+    #     """Returns lines that match a pattern in the files of the provided Directory"""
+    #     return await (
+    #         dag.container()
+    #         .from_("alpine:latest")
+    #         .with_mounted_directory("/mnt", directory_arg)
+    #         .with_workdir("/mnt")
+    #         .with_exec(["grep", "-R", pattern, "."])
+    #         .stdout()
+    #     )
