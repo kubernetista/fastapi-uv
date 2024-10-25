048704cc73f994dc5142a7ec95d4d3fa83575a30
Alfredo Colangelo <6275539+acola@users.noreply.github.com>
2024-10-25 15:27:40 +0200

    dager: adding test in progress

--- dagger/src/main/__init__.py
+++ dagger/src/main/__init__.py
@@ -10,7 +10,22 @@
 from typing import Annotated
 
 import dagger
-from dagger import Doc, dag, function, object_type
+from dagger import DaggerError, Doc, dag, field, function, object_type
+
+SCRIPT = """#!/bin/sh
+echo "Test Suite"
+echo "=========="
+echo "Test 1: PASS" >> report.txt
+echo "Test 2: FAIL" >> report.txt
+echo "Test 3: PASS" >> report.txt
+exit 0
+"""
+
+
+@object_type
+class TestResult:
+    report: dagger.File = field()
+    exit_code: str = field()
 
 
 @object_type
@@ -54,3 +69,75 @@ class FastapiUv:
         container = await self.build(src)
         image_name = f"{registry}/{path}/{image}:{tag}"
         return await container.with_registry_auth(registry, username, password).publish(image_name)
+
+    @function
+    async def test(
+        self,
+        src: Annotated[
+            dagger.Directory,
+            Doc("root directory of the project"),
+        ],
+    ) -> TestResult:
+        """Handle errors"""
+        try:
+            ctr = await (
+                dag.container()
+                # .from_("python:3.12-alpine")
+                .from_("python:3.12")
+                # .with_exec(["sh", "-c", "apk add --no-cache curl bash"])
+                # .with_exec(["sh", "-c", "apt-get update && apt-get install -y curl bash"])
+                .with_exec([
+                    "sh",
+                    "-c",
+                    "curl -LsSf https://astral.sh/uv/install.sh | env UV_INSTALL_DIR='/usr/local' sh",
+                ])
+                .with_directory("/src", src)
+                .with_workdir("/src")
+                # add script with execution permission to simulate a testing tool.
+                # .with_new_file("run-tests", SCRIPT, permissions=0o750)
+                # .terminal()
+                # .terminal("/bin/bash")
+                # .exec(["/usr/bin/bash"])
+                # .terminal(["/usr/bin/bash"])
+                # if the exit code isn't needed: "run-tests; true"
+                .with_exec(["bash", "-c", "uv lock --locked"])
+                .with_exec(["bash", "-c", "uv run pre-commit run --all-files"])
+                .with_exec(["bash", "-c", "uv run mypy ./src"])
+                .with_exec(["bash", "-c", "uv run deptry ./src"])
+                .with_exec(["bash", "-c", "uv run --with pyright pyright ./src"])
+                # the result of `sync` is the container, which allows continued chaining
+                .sync()
+            )
+
+            # save report for inspection.
+            report = ctr.file("report.txt")
+
+            # use the saved exit code to determine if the tests passed.
+            exit_code = await ctr.file("exit_code").contents()
+
+            return TestResult(report=report, exit_code=exit_code)
+        except DaggerError as e:
+            # DaggerError is the base class for all errors raised by Dagger
+            msg = "Unexpected Dagger error"
+            raise RuntimeError(msg) from e
+
+    @function
+    async def test_test(
+        self,
+        src: Annotated[
+            dagger.Directory,
+            Doc("location of directory containing Dockerfile"),
+        ],
+    ) -> str:
+        """Test function"""
+        ref = (
+            dag.container()
+            .with_directory(".", src)
+            .with_workdir("/")
+            .directory(".")
+            .docker_build()  # build from Dockerfile
+        )
+        return await ref
+
+
+# ruff: noqa: RET505
