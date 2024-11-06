43ff1242878f3230396b494dff93c77db8c492dc
Alfredo Colangelo <6275539+acola@users.noreply.github.com>
2024-11-06 13:25:35 +0100

    main.py: add {"message": "OK"} fallback for tests

--- src/fastapi_uv/main.py
+++ src/fastapi_uv/main.py
@@ -27,17 +27,19 @@ metadata = StandardMetadata.from_pyproject(
 #         # Handle the cancellation gracefully
 #         pass
 
+
 @app.get("/")
 def get_root() -> dict[str, str]:
+    # return {"message": "OK"}
     return {
         "status": "OK",
         "app-name": metadata.name,
         "version": str(metadata.version),
     }
 
+
 if __name__ == "__main__":
     host = os.getenv("HOST", "0.0.0.0")  # noqa: S104  # nosec B104
     port = int(os.getenv("PORT", 8001))
     uvicorn.run(app, host=host, port=port)
     # uvicorn.run(app, host=host, port=port, lifespan="off")
-
